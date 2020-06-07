9906c603729bda3f443850e219d69faa6c9b03ca
slopey112 <howard_peng@icloud.com>
2020-06-06 21:49:05 -0700

    add bootloader offset

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,5 +1,20 @@
-loop:
-	jmp loop
+[org 0x7c00]
+
+mov ah, 0x0e
+mov al, "s"
+int 0x10
+mov al, "l"
+int 0x10
+mov al, "o"
+int 0x10
+mov al, "n"
+int 0x10
+mov al, "i"
+int 0x10
+mov al, "x"
+int 0x10
+
+jump $
 
 times 510-($-$$) db 0
 dw 0xaa55
